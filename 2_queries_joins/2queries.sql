bbf0f89a0e7339cc1e7496d9e352edf7152a3616
Harrish S <harrish.s@hotmail.ca>
2019-09-17 17:25:22 +0000

    create queries using join

---
+++ 2_queries_joins/2queries.sql
@@ -0,0 +1,12 @@
+-- total assignment duration
+SELECT sum(assignment_submissions.duration) as total_duration
+FROM assignment_submissions
+  JOIN students ON students.id = student_id
+WHERE students.name = "Ibrahim Schimmel";
+
+-- Cohort total duration
+SELECT sum(assignment_submissions.duration) as total_duration
+FROM assignment_submissions
+  JOIN students ON students.id = student_id
+  JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name = 'FEB12';
